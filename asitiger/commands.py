95446857fbd0875ba0cbd7b6493ef33a61cdbc7a
Chili Johnson <chilij@chilij.com>
2020-10-27 23:32:12 -0700

    Adds a small set of initial commands to experiment with.

---
+++ asitiger/commands.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Commands(Enum):
+    HERE = "H"
+    HOME = "!"
+    LED = "LED"
+    MOVE = "M"
+    RDSTAT = "RS"
+    SETHOME = "HM"
+    STATUS = "/"
+    WHO = "WHO"
