5fffb1b8ea612012c5148c609451523f3cef5a2d
Chili Johnson <chilij@chilij.com>
2020-11-03 16:05:10 -0800

    Updates the package info with a proper description and classifiers.

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 import setuptools
 
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+
 setuptools.setup(
     name="asitiger",
-    version="0.0.1",
+    version="0.0.3",
     author="Chili Johnson",
-    author_email="chilijohnson@chilij.com",
-    description="A thin library for controlling ASI Tiger Controllers",
-    url="https://github.com/ChiliJohnson/asitiger",
+    author_email="chilij@chilij.com",
+    description="A thin Python interface for communicating with ASI Tiger Controllers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/System1Bio/asitiger",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
-    install_requires=["pyserial>=3.4"],
+    install_requires=["pyserial>=3.0"],
     extras_require={
         "dev": ["black==19.10b0", "flake8-bugbear==19.8.0", "pre-commit>=2.7.1"]
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
